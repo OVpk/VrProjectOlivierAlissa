27b1f8bc267f11bac594d96ecccdee2263e7c985
VERFAILLIE Olivier <o.verfaillie@rubika-edu.com>
2025-10-14 12:40:44 +0200

    sequence and card

--- Assets/Game/Script/CardData.cs
+++ Assets/Game/Script/CardData.cs
@@ -7,6 +7,12 @@ public enum CardColors
     Blue
 }
 
+public enum CardState
+{
+    Declaration,
+    Play
+}
+
 public class CardData : ScriptableObject
 {
     [SerializeField] public CardColors color {get; private set;}
@@ -17,9 +23,9 @@ public class CardData : ScriptableObject
     
     [SerializeField] public AudioClip playSound {get; private set;}
 
-    public CardDataInstance Instance()
+    public CardDataInstance Instance(CardState state)
     {
-        return new CardDataInstance(this);
+        return new CardDataInstance(this, state);
     }
     
 }
@@ -30,12 +36,14 @@ public class CardDataInstance
     public Sprite visual;
     public AudioClip declarationSound;
     public AudioClip playSound;
+    public CardState cardState;
     
-    public CardDataInstance(CardData data)
+    public CardDataInstance(CardData data, CardState state)
     {
         color = data.color;
         visual = data.visual;
         declarationSound = data.declarationSound;
         playSound = data.playSound;
+        cardState = state;
     }
 }
--- Assets/Game/Script/Sequence.cs
+++ Assets/Game/Script/Sequence.cs
@@ -2,11 +2,16 @@ using UnityEngine;
 
 public class Sequence : MonoBehaviour
 {
-    private CardDataInstance[] beats;
+    public CardDataInstance[] beats { get; private set; }
 
-    public Sequence(int nbOfBeats)
+    public Sequence(int nbOfBeats, CardData card)
     {
         beats = new CardDataInstance[nbOfBeats];
+        for (int i = 0; i < nbOfBeats-1; i++)
+        {
+            beats[i] = card.Instance(CardState.Declaration);
+        }
+        beats[nbOfBeats] = card.Instance(CardState.Play);
     }
     
     
