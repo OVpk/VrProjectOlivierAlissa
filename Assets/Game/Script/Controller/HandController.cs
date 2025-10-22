4ccf46493e4bedf5bdbc8573be26798906c9aa37
DELATTRE Alissa <a.delattre@rubika-edu.com>
2025-10-22 15:10:54 +0200

    pouvoir lancer carte (a bosser)

--- Assets/Game/Script/Controller/HandController.cs
+++ Assets/Game/Script/Controller/HandController.cs
@@ -1,5 +1,6 @@
 using DG.Tweening;
 using System.ComponentModel.Design;
+using System.Linq.Expressions;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,12 +11,17 @@ public class HandController : MonoBehaviour
     [SerializeField] private LayerMask cardValideLayers;
     [SerializeField] private EnumHand currentHand;
     [SerializeField] private GameObject anchor;
+    [SerializeField] private Transform tableTarget;
+    [SerializeField] private float throwInfluence = 0.4f; 
+    [SerializeField] private float throwForce = 8f;
 
     private Collider cardCollider;
     private GameObject cardInHand;
     private Rigidbody cardRB;
     private DroppedCard cardDropped;
     private int cardIndex = 0;
+    private Vector3 lastAnchorPos;
+    private Vector3 anchorVelocity;
 
     private void OnEnable()
     {
@@ -44,13 +50,28 @@ public class HandController : MonoBehaviour
         cardInHand.SetActive(false);
     }
 
+    private void Update()
+    {
+        if (!cardInHand.activeInHierarchy || cardDropped.IsDropped)
+            return;
+
+        Vector3 currentPos = anchor.transform.position;
+        anchorVelocity = (currentPos - lastAnchorPos) / Time.deltaTime;
+        lastAnchorPos = currentPos;
+
+    }
+
     private void ResetCardPosition()
     {
         cardInHand.transform.parent = anchor.transform;
         cardDropped.IsDropped = false;
         cardInHand.transform.localPosition = Vector3.zero;
         cardInHand.transform.rotation = Quaternion.identity;
-        cardRB.isKinematic = true;
+
+        cardRB.linearVelocity = Vector3.zero;
+        cardRB.angularVelocity = Vector3.zero;
+        cardRB.useGravity = false;
+
     }
 
     private void SpawnCard(EnumHand pHand)
@@ -67,10 +88,10 @@ public class HandController : MonoBehaviour
 
     private void ChangeCard(EnumHand pHand)
     {
-        if (currentHand != pHand || !cardInHand.activeInHierarchy)
+        if (currentHand != pHand || !cardInHand.activeInHierarchy || cardDropped.IsDropped)
             return;
 
-        cardIndex = (cardIndex +1) % cards.Length;
+        cardIndex = (cardIndex + 1) % cards.Length;
         SpawnCard(pHand);
     }
 
@@ -79,21 +100,27 @@ public class HandController : MonoBehaviour
         if (currentHand != pHand || !cardInHand.activeInHierarchy)
             return;
 
-        RaycastHit hit;
-        if (Physics.Raycast(cardInHand.transform.position, Vector3.down, out hit, 10f, cardValideLayers))
-        {
-            cardInHand.transform.parent = null;
-            cardRB.isKinematic = false;
-            cardCollider.enabled = true;
-            cardRB.DORotate(new Vector3(90f, cardRB.transform.rotation.eulerAngles.y, cardRB.transform.rotation.eulerAngles.z), 0.2f);
+        cardInHand.transform.parent = null;
 
-            cardDropped.IsDropped = true;
-            cardDropped.cardData = cards[cardIndex].Instance(CardState.Play);
-        }
-        else
+        cardRB.useGravity = true;
+        cardRB.isKinematic = false;
+        cardCollider.enabled = true;
+
+        float handSpeed = anchorVelocity.magnitude;
+        
+
+        if (handSpeed > 0.35f)
         {
-            DeactivateCard();
+            Vector3 toTable = (tableTarget.position - anchor.transform.position).normalized;
+            Vector3 handDir = anchorVelocity.normalized;
+            Vector3 finalDir = Vector3.Slerp(toTable, handDir, throwInfluence).normalized;
+            cardRB.AddForce(finalDir * throwForce, ForceMode.VelocityChange);
         }
+
+        cardRB.DORotate(new Vector3(90f, cardRB.transform.rotation.eulerAngles.y, cardRB.transform.rotation.eulerAngles.z), 0.2f);
+
+        cardDropped.IsDropped = true;
+        cardDropped.cardData = cards[cardIndex].Instance(CardState.Play);
     }
 
     private void DeactivateCard() => cardInHand.SetActive(false);
