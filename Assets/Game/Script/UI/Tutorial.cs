2be75b9ca999f412a1fefdc363450b71b03a187b
DELATTRE Alissa <a.delattre@rubika-edu.com>
2025-11-12 15:04:35 +0100

    debut tuto

---
+++ Assets/Game/Script/UI/Tutorial.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tutorial : MonoBehaviour
+{
+    [SerializeField] Image background;
+    [SerializeField] Image cardDeclaration;
+    [SerializeField] Image cardShoot;
+    [SerializeField] Image cardPlay;
+}
